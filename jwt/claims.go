a95a65c70c05b781421318178fd7ed60bd6d1f09
Dmytro Brykovets <dmytro.brykovets@gmail.com>
2020-09-16 11:41:57 +0300

    convert jwt user id to string

--- jwt/claims.go
+++ jwt/claims.go
@@ -1,8 +1,53 @@
 package jwt
 
-import "github.com/dgrijalva/jwt-go"
+import (
+	"encoding/json"
+	"strconv"
+
+	"github.com/dgrijalva/jwt-go"
+)
 
 type Claims struct {
 	UserID string `json:"id"`
 	jwt.StandardClaims
+}
+
+// UnmarshalJSON takes care of converting Claims.UserID to a string if it came as an integer.
+func (c *Claims) UnmarshalJSON(data []byte) error {
+	// Standart claims first.
+	sc := jwt.StandardClaims{}
+	err := json.Unmarshal(data, &sc)
+	if err != nil {
+		return err
+	}
+	c.StandardClaims = sc
+
+	// Try to unmarshal id as a string.
+	idStr := struct {
+		ID string `json:"id"`
+	}{}
+	err = json.Unmarshal(data, &idStr)
+	if err == nil {
+		// Successful unmarshaling. Continuing the normal flow.
+		c.UserID = idStr.ID
+	} else {
+		// Try to unmarshal id as an integer if this field caused error.
+		if err, ok := err.(*json.UnmarshalTypeError); ok && err.Field == "id" {
+			idInt := struct {
+				ID int64 `json:"id"`
+			}{}
+			err := json.Unmarshal(data, &idInt)
+			// Give up trying.
+			if err != nil {
+				return err
+			}
+
+			// Set UserID as a string.
+			c.UserID = strconv.FormatInt(idInt.ID, 10)
+		} else {
+			// Error is not caused by id field. Returning.
+			return err
+		}
+	}
+	return nil
 }
--- jwt/jwt_test.go
+++ jwt/jwt_test.go
@@ -1,8 +1,6 @@
 package jwt
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
 	"reflect"
 	"testing"
 
@@ -10,27 +8,72 @@ import (
 )
 
 func TestParse(t *testing.T) {
-	key, _ := rsa.GenerateKey(rand.Reader, 2048)
-
-	t.Run("Should parse a valid token", func(t *testing.T) {
+	// key, _ := rsa.GenerateKey(rand.Reader, 2048)
+	// token := jwt.NewWithClaims(jwt.SigningMethodRS256, wantClaims)
+	// tokenString, err := token.SignedString(key)
+	// if err != nil {
+	// 	t.Errorf("unexpected error: %v", err)
+	// 	return
+	// }
+	t.Run("Should parse token with string user id", func(t *testing.T) {
+		alg := "HS256"
+		secret := []byte("secret")
+		// { id: "123456789" }
+		token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEyMzQ1Njc4OSJ9.XMgTcgoDgqbxLvDQG6h6EjthqMhb4KBFY-nnYzAq-og"
 		wantClaims := &Claims{
-			UserID: "1",
+			UserID: "123456789",
 		}
 
-		token := jwt.NewWithClaims(jwt.SigningMethodRS256, wantClaims)
-		tokenString, err := token.SignedString(key)
-		if err != nil {
-			t.Errorf("unexpected error: %v", err)
-			return
-		}
-
-		got, err := Parse(tokenString, "RS256", key.Public())
+		got, err := Parse(token, alg, secret)
 		if err != nil {
 			t.Errorf("Parse() unexpected error: %v", err)
 			return
 		}
 		if !reflect.DeepEqual(got, wantClaims) {
-			t.Errorf("Parse() = %v, want %v", got, wantClaims)
+			t.Errorf("Parse() = %#v, want %#v", got, wantClaims)
+		}
+		t.Logf("got: %#v", got)
+	})
+
+	t.Run("Should parse token with int user id", func(t *testing.T) {
+		alg := "HS256"
+		secret := []byte("secret")
+		// { id: 123456789 }
+		token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTIzNDU2Nzg5fQ.xNQJDKQz08Zm1-MTTQNLj34CFu51WAm_WpRwnF9eAFU"
+		wantClaims := &Claims{
+			UserID: "123456789",
+		}
+
+		got, err := Parse(token, alg, secret)
+		if err != nil {
+			t.Errorf("Parse() unexpected error: %v", err)
+			return
+		}
+		if !reflect.DeepEqual(got, wantClaims) {
+			t.Errorf("Parse() = %#v, want %#v", got, wantClaims)
+		}
+		t.Logf("got: %#v", got)
+	})
+
+	t.Run("Should parse token with standart claims", func(t *testing.T) {
+		alg := "HS256"
+		secret := []byte("secret")
+		// { id: "123456789", iat: 1516239022 }
+		token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEyMzQ1Njc4OSIsImlhdCI6MTUxNjIzOTAyMn0.SQiePc7E2ZJp8PjEU2AjcJ7JsD6_sT_gEUeg3XWHQt0"
+		wantClaims := &Claims{
+			UserID: "123456789",
+			StandardClaims: jwt.StandardClaims{
+				IssuedAt: 1516239022,
+			},
+		}
+
+		got, err := Parse(token, alg, secret)
+		if err != nil {
+			t.Errorf("Parse() unexpected error: %v", err)
+			return
+		}
+		if !reflect.DeepEqual(got, wantClaims) {
+			t.Errorf("Parse() = %#v, want %#v", got, wantClaims)
 		}
 		t.Logf("got: %#v", got)
 	})
